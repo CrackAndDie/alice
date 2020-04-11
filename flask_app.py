9abb4d75c5796e80bb8071bdda309ffe9d20a3a8
Airat <52558686+CrackAndDie@users.noreply.github.com>
2020-04-11 14:11:08 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -49,6 +49,7 @@ def handle_dialog(res, req):
         res['response']['text'] = 'Привет, назови свое имя!'
         sessionStorage[user_id] = {
             'first_name': None,
+            'first_name_up': None,
             'game_started': False,
             'talks': 1
         }
@@ -60,6 +61,7 @@ def handle_dialog(res, req):
                 res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
             else:
                 sessionStorage[user_id]['first_name'] = first_name
+                sessionStorage[user_id]['first_name_up'] = first_name[0].upper() + first_name[1:]
                 sessionStorage[user_id]['guessed_cities'] = []
                 res['response']['text'] = f'Приятно познакомиться, {first_name.title()}. Я - Алиса, а из какого ты города?'
                 res['response']['buttons'] = [
@@ -96,7 +98,7 @@ def handle_dialog(res, req):
                 if not sessionStorage[user_id]['game_started']:
                     if 'да' in req['request']['nlu']['tokens']:
                         if len(sessionStorage[user_id]['guessed_cities']) == 3:
-                            res['response']['text'] = f'{sessionStorage[user_id]["first_name"]}, все города угаданы, спасибо за игру!'
+                            res['response']['text'] = f'{sessionStorage[user_id]["first_name_up"]}, все города угаданы, спасибо за игру!'
                             res['end_session'] = True
                         else:
                             sessionStorage[user_id]['game_started'] = True
@@ -104,10 +106,10 @@ def handle_dialog(res, req):
                             sessionStorage[user_id]['step'] = 1
                             play_game(res, req)
                     elif 'нет' in req['request']['nlu']['tokens']:
-                        res['response']['text'] = f'До свидания, {sessionStorage[user_id]["first_name"]}!'
+                        res['response']['text'] = f'До свидания, {sessionStorage[user_id]["first_name_up"]}!'
                         res['end_session'] = True
                     else:
-                        res['response']['text'] = f'{sessionStorage[user_id]["first_name"]}, не поняла ответа. Да или нет?'
+                        res['response']['text'] = f'{sessionStorage[user_id]["first_name_up"]}, не поняла ответа. Да или нет?'
                         res['response']['buttons'] = [
                             {
                                 'title': 'да',
@@ -137,14 +139,14 @@ def play_game(res, req):
         sessionStorage[user_id]['city'] = city
         res['response']['card'] = {}
         res['response']['card']['type'] = 'BigImage'
-        res['response']['card']['title'] = f'{sessionStorage[user_id]["first_name"]}, какой это город?'
+        res['response']['card']['title'] = f'{sessionStorage[user_id]["first_name_up"]}, какой это город?'
         res['response']['card']['image_id'] = cities[city][attempt - 1]
         res['response']['text'] = 'Я угадал!'
         sessionStorage[user_id]['step'] = 2
     elif step == 2:
         city = sessionStorage[user_id]['city']
         if get_city(req) == city:
-            res['response']['text'] = f'Правильно. {sessionStorage[user_id]["first_name"]}, a в какой стране этот город?'
+            res['response']['text'] = f'Правильно. {sessionStorage[user_id]["first_name_up"]}, a в какой стране этот город?'
             sessionStorage[user_id]['guessed_cities'].append(city)
             sessionStorage[user_id]['game_started'] = True
             if res['response'].get('buttons', 'gg') != 'gg':
@@ -164,7 +166,7 @@ def play_game(res, req):
             return
         else:
             if attempt == 3:
-                res['response']['text'] = f'{sessionStorage[user_id]["first_name"]}, Вы пытались. Это {city.title()}. А в какой стране этот город?'
+                res['response']['text'] = f'{sessionStorage[user_id]["first_name_up"]}, Вы пытались. Это {city.title()}. А в какой стране этот город?'
                 sessionStorage[user_id]['game_started'] = True
                 sessionStorage[user_id]['guessed_cities'].append(city)
                 if res['response'].get('buttons', 'gg') != 'gg':
@@ -185,14 +187,14 @@ def play_game(res, req):
             else:
                 res['response']['card'] = {}
                 res['response']['card']['type'] = 'BigImage'
-                res['response']['card']['title'] = f'{sessionStorage[user_id]["first_name"]}, не верно, попробуйте еще раз'
+                res['response']['card']['title'] = f'{sessionStorage[user_id]["first_name_up"]}, не верно, попробуйте еще раз'
                 res['response']['card']['image_id'] = cities[city][attempt - 1]
                 res['response']['text'] = 'Я угадал!'
     elif step == 3:
         city = sessionStorage[user_id]['city']
         count = counts[city]
         if get_count(req) == count:
-            res['response']['text'] = f'Правильно. {sessionStorage[user_id]["first_name"]}, cыграем еще?'
+            res['response']['text'] = f'Правильно. {sessionStorage[user_id]["first_name_up"]}, cыграем еще?'
             sessionStorage[user_id]['game_started'] = False
             if res['response'].get('buttons', 'gg') != 'gg':
                 res['response']['buttons'].append({
@@ -210,7 +212,7 @@ def play_game(res, req):
             return
         else:
             if attempt == 3:
-                res['response']['text'] = f'{sessionStorage[user_id]["first_name"]}, Вы пытались. Это {count}. Сыграем еще?'
+                res['response']['text'] = f'{sessionStorage[user_id]["first_name_up"]}, Вы пытались. Это {count}. Сыграем еще?'
                 sessionStorage[user_id]['game_started'] = False
                 if res['response'].get('buttons', 'gg') != 'gg':
                     res['response']['buttons'].append({
@@ -227,7 +229,7 @@ def play_game(res, req):
                 sessionStorage[user_id]['step'] = 1
                 return
             else:
-                res['response']['text'] = f'Неверно! {sessionStorage[user_id]["first_name"]}, попробуйте еще раз'
+                res['response']['text'] = f'Неверно! {sessionStorage[user_id]["first_name_up"]}, попробуйте еще раз'
     sessionStorage[user_id]['attempt'] += 1
 
 
